2bac152fa21b1bc678227e63645dead6ddf62eb6
saulocosta <saulo.costa@gmail.com>
2023-08-20 19:27:37 -0300

    Projeto MySql/JPA

---
+++ src/main/java/saulo/io/projeto1/ProjetoMySqlApplication.java
@@ -0,0 +1,13 @@
+package saulo.io.projeto1;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProjetoMySqlApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProjetoMySqlApplication.class, args);
+	}
+
+}
---
+++ src/test/java/saulo/io/projeto1/ProjetoMySqlApplicationTests.java
@@ -0,0 +1,13 @@
+package saulo.io.projeto1;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProjetoMySqlApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
